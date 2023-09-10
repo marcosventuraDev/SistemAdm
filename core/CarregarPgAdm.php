34d07a7c69ba681dd48cabe3775172b737c7efd8
marcos ventura <marcosventura.dev@gmail.com>
2023-09-10 15:44:19 -0300

    Crinado página restrita

--- core/CarregarPgAdm.php
+++ core/CarregarPgAdm.php
@@ -8,53 +8,85 @@ class CarregarPgAdm
     private string $urlMetodo;
     private string $urlParameter;
     private string $classLoad;
-  
+    private array $listPgPublic;
+    private array $listPgPrivate;
 
-    public function loadPage(string|null $urlController, string|null  $urlMetodo, string|null $urlParameter):void
+    private string $urlSlugController;
+    private string $urlSlugMetodo;
+
+
+    public function loadPage(string|null $urlController, string|null  $urlMetodo, string|null $urlParameter): void
     {
         $this->urlController = $urlController;
         $this->urlMetodo = $urlMetodo;
         $this->urlParameter = $urlParameter;
-   
 
-        $this->classLoad = "\\App\\adms\\Controllers\\" . $this->urlController;
-        if(class_exists($this->classLoad)){
-           $this->loadMetodo();
-        }else{
+        $this->pgPublic();
+
+        if (class_exists($this->classLoad)) {
+            $this->loadMetodo();
+        } else {
             die("Erro - 003: Por favor tente novamente. Caso o problema persista, entre em contato o administrador " . ADM);
-         /*    $this->urlController = $this->slugController(CONTROLLER);
-            $this->urlMetodo = $this->slugController(METODO);
-            $this->urlParameter = "";
-            $this->loadPage($this->urlController,  $this->urlMetodo, $this->urlParameter); */
         }
     }
 
-private function loadMetodo():void
-{
-    $classLoad = new $this->classLoad();
-    if(method_exists($classLoad, $this->urlMetodo)){
-       $classLoad->{$this->urlMetodo}();
-    }else{
-        die("<p style='color:red'>ERRO: Por favor tente novamente. Caso o erro persista, entre em contato com o administrador <a href='#'>".ADM ."</a></p>");
+    private function loadMetodo(): void
+    {
+        $classLoad = new $this->classLoad();
+        if (method_exists($classLoad, $this->urlMetodo)) {
+            $classLoad->{$this->urlMetodo}();
+        } else {
+            die("<p style='color:red'>ERRO: Por favor tente novamente. Caso o erro persista, entre em contato com o administrador <a href='#'>" . ADM . "</a></p>");
+        }
     }
 
-}
 
+    private function pgPublic(): void
+    {
+        $this->listPgPublic = ["Login"];
 
+        if (in_array($this->urlController, $this->listPgPublic)) {
+            $this->classLoad = "\\App\\adms\\Controllers\\" . $this->urlController;
+        } else {
+            $this->PgPrivate();
+        }
+    }
+
+    private function pgPrivate(): void
+    {
+        $this->listPgPrivate = ["Dashboard"];
+        if (in_array($this->urlController, $this->listPgPrivate)) {
+            $this->verifyLogin();
+        } else {
+            $_SESSION['msg'] = "<p style='color:red'>Erro: Página não encontrada!<p/>";
+            $urlRedirect =  URLADM . "login/index";
+            header("Location: $urlRedirect");
+        }
+    }
+
+    private function verifyLogin(): void
+    {
+        if ((isset($_SESSION['user_id'])) and (isset($_SESSION['user_name'])) and (isset($_SESSION['user_email']))) {
+            $this->classLoad = "\\App\\adms\\Controllers\\" . $this->urlController;
+        } else {
+            $_SESSION['msg'] = "<p style='color:red'>Erro: Para acessar a página realize o login!<p/>";
+            $urlRedirect =  URLADM . "login/index";
+            header("Location: $urlRedirect");
+        }
+    }
 
 
     //Deverá ser inserido em um arquivo helper
     public function slugController($slugController): string
     {
         $this->urlSlugController = $slugController;
-        $this->urlSlugController= strtolower($this->urlSlugController);
-        $this->urlSlugController = str_replace("-"," ", $this->urlSlugController);
+        $this->urlSlugController = strtolower($this->urlSlugController);
+        $this->urlSlugController = str_replace("-", " ", $this->urlSlugController);
         $this->urlSlugController = ucwords($this->urlSlugController);
-        $this->urlSlugController = str_replace(" ","", $this->urlSlugController);
-         
-      
-        return $this->urlSlugController;
+        $this->urlSlugController = str_replace(" ", "", $this->urlSlugController);
 
+
+        return $this->urlSlugController;
     }
 
     public function slugMetodo($slugMetodo): string
@@ -63,5 +95,4 @@ private function loadMetodo():void
         $this->urlSlugMetodo = lcfirst($this->urlSlugMetodo);
         return $this->urlSlugMetodo;
     }
-
 }
