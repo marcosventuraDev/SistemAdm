7f62a17cd5b9d48f00e9273a550b7dfb8b96f15c
marcos ventura <marcosventura.dev@gmail.com>
2023-09-13 06:16:06 -0300

    implementando validação de força de senha com javaScript

--- app/adms/assets/js/custom_adms.js
+++ app/adms/assets/js/custom_adms.js
@@ -2,32 +2,51 @@
 //validação de cadastro de usuário
 const formNewUser = document.getElementById("form-new-user");
 
-if(formNewUser){
+if (formNewUser) {
 
     //quando for submetido à um evento o formulario de id form-new-user
-    formNewUser.addEventListener("submit", async(e)=>{
+    formNewUser.addEventListener("submit", async (e) => {
 
         //recebe os valores das variáveis no campos correspondente ao ID
         var name = document.querySelector('#name').value;
         var email = document.querySelector('#email').value;
         var password = document.querySelector('#password').value;
 
-         //verifica se o campo name está vazio
-        if(name === ""){
+        //verifica se o campo name está vazio
+        if (name === "") {
             e.preventDefault();
             document.getElementById("msg").innerHTML = "<p style='color:red'>Erro: Necessário Preencher o campo nome!</p>"
             return
         }
-         //verifica se o campo email está vazio
-        if(email === ""){
+        //verifica se o campo email está vazio
+        if (email === "") {
             e.preventDefault();
             document.getElementById("msg").innerHTML = "<p style='color:red'>Erro: Necessário Preencher o campo E-mail!</p>"
             return
         }
         //verifica se o campo password está vazio
-        if(password === ""){
+        if (password === "") {
             e.preventDefault();
             document.getElementById("msg").innerHTML = "<p style='color:red'>Erro: Necessário Preencher o campo Password!</p>"
+            return
+        }
+        //a senha deve conter 6 ou mais caractéres
+        if (password.length < 6) {
+            e.preventDefault();
+            document.getElementById("msg").innerHTML = "<p style='color:red'>Erro: A senha deve ter no mínimo 6 caracteres</p>"
+            return
+        }
+        // a senha não pode ter números repetidos
+        if (password.match(/([(1-9)]+)\1{2,}/)) {
+            e.preventDefault();
+            document.getElementById("msg").innerHTML = "<p style='color:red'>Erro: A senha possui muitos números sequenciais repetidos</p>"
+            return
+        }
+
+        //verifica se o campo senha não possui letras
+        if (!password.match(/[(A-Za-z)]/)) {
+            e.preventDefault();
+            document.getElementById("msg").innerHTML = "<p style='color:red'>Erro: A senha deve ter pelo menos 1 letra</p>"
             return
         }
 
@@ -36,19 +55,19 @@ if(formNewUser){
 
 //Validação do login
 const formLorgin = document.getElementById("form-login");
-if(formLorgin){
+if (formLorgin) {
 
-    formLorgin.addEventListener("submit", async(e)=>{
+    formLorgin.addEventListener("submit", async (e) => {
         e.preventDefault();
-    
+
         var user = document.querySelector('#user').value;
         var password = document.querySelector('#password').value;
-        if(user === ""){
+        if (user === "") {
             e.preventDefault();
             document.getElementById("msg").innerHTML = "<p style='color:red'>Erro: Necessário Preencher o campo E-mail!</p>"
             return
         }
-        if(password === ""){
+        if (password === "") {
             e.preventDefault();
             document.getElementById("msg").innerHTML = "<p style='color:red'>Erro: Necessário Preencher o campo password!</p>"
             return
